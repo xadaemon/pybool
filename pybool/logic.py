2113c16004b5a31c06a06542ee093b79811db6fe
xadaemon <mxavierdev@gmail.com>
2025-09-19 22:47:44 +0200

    add operator overloadings

--- pybool/logic.py
+++ pybool/logic.py
@@ -2,6 +2,7 @@ from copy import copy
 from enum import Enum
 from typing import override
 
+
 # Valid logic values
 class LogicValue(Enum):
     BFalse = 0
@@ -13,19 +14,18 @@ class LogicValue(Enum):
     def from_bool(v: bool) -> "LogicValue":
         return LogicValue.BTrue if v else LogicValue.BFalse
 
+
 LV = LogicValue
 
-BoolOp = Enum("BooleanOperation", [
-    ("AND", 0),
-    ("OR", 1),
-    ("XOR", 2),
-    ("NAND", 3),
-    ("NOR", 4),
-    ("NOT", 5)
-])
+BinOp = Enum(
+    "BooleanOperation",
+    [("AND", 0), ("OR", 1), ("XOR", 2), ("NAND", 3), ("NOR", 4)],
+)
+
 
 class Logic:
     val: LogicValue = LogicValue.X
+
     def __init__(self, val: LogicValue | bool = LV.X) -> None:
         if isinstance(val, bool):
             val = LogicValue.from_bool(val)
@@ -35,13 +35,13 @@ class Logic:
     def __repr__(self) -> str:
         match self.val:
             case LV.BFalse:
-                return '0'
+                return "0"
             case LV.BTrue:
-                return '1'
+                return "1"
             case LV.X:
-                return 'x'
+                return "x"
             case LV.Z:
-                return 'z'
+                return "z"
 
     def _is_logic_val(self) -> bool:
         return not self.val in [LV.X, LV.Z]
@@ -57,22 +57,27 @@ class Logic:
             raise ValueError("X and Z state cannot be converted to boolean")
         return True if self.val == LV.BTrue else False
 
-    def bool_op(self, other: "Logic", op: BoolOp) -> "Logic":
+    def bin_op(self, other: "Logic", op: BinOp) -> "Logic":
         if not self._is_logic_val():
             raise ValueError("Cannot do operations on X and Z signals")
         match op:
-            case BoolOp.AND:
+            case BinOp.AND:
                 return Logic(LV.BTrue if self and other else LV.BFalse)
-            case BoolOp.OR:
+            case BinOp.OR:
                 return Logic(LV.BTrue if self or other else LV.BFalse)
-            case BoolOp.XOR:
-                return Logic(LV.BTrue if (self or other) and (not (self and other)) else LV.BFalse)
-            case BoolOp.NAND:
+            case BinOp.XOR:
+                return Logic(
+                    LV.BTrue
+                    if (self or other) and (not (self and other))
+                    else LV.BFalse
+                )
+            case BinOp.NAND:
                 return Logic(LV.BTrue if not (self and other) else LV.BFalse)
-            case BoolOp.NOR:
+            case BinOp.NOR:
                 return Logic(LV.BTrue if not (self or other) else LV.BFalse)
-            case BoolOp.NOT:
-                return Logic(LV.BFalse if self.val == LV.BTrue else LV.BTrue)
+
+    def negate(self):
+        return Logic(LV.BFalse if self.val == LV.BTrue else LV.BTrue)
 
     @override
     def __str__(self) -> str:
@@ -86,7 +91,8 @@ class Logic:
 L_TRUE = Logic(LV.BTrue)
 L_FALSE = Logic(LV.BFalse)
 
-_logic_vec_default = Logic.n_of_val(8)
+_logic_vec_default = Logic.n_of_val(8, LV.BFalse)
+
 
 class LogicVector:
     width: int = 0
@@ -111,6 +117,76 @@ class LogicVector:
             val = Logic(val)
         self._vals[item] = val
 
-    def bool_op(self, other: "LogicVector", op: BoolOp) -> "LogicVector":
-        return LogicVector(self.width, [vs.bool_op(vo, op) for vs, vo in zip(self._vals, other._vals)])
+    def bin_op(self, other: "LogicVector", op: BinOp) -> "LogicVector":
+        if other.width != self.width:
+            other = other.extend(self.width)
+        return LogicVector(
+            self.width, [vs.bin_op(vo, op) for vs, vo in zip(self._vals, other._vals)]
+        )
 
+    def __or__(self, value: "LogicVector") -> "LogicVector":
+        return self.bin_op(value, BinOp.OR)
+
+    def __and__(self, value: "LogicVector") -> "LogicVector":
+        return self.bin_op(value, BinOp.AND)
+
+    def __xor__(self, value: "LogicVector") -> "LogicVector":
+        return self.bin_op(value, BinOp.XOR)
+
+    def __invert__(self) -> "LogicVector":
+        return self.negate()
+
+    def negate(self) -> "LogicVector":
+        return LogicVector(self.width, [v.negate() for v in self._vals])
+
+    def extend(self, by: int) -> "LogicVector":
+        diff = by - self.width
+        ext = Logic.n_of_val(diff, LV.BFalse)
+        vals = copy(self._vals)
+        vals.extend(ext)
+        return LogicVector(self.width, vals)
+
+    def sll(self, shift: int) -> "LogicVector":
+        shifted = Logic.n_of_val(self.width, LV.BFalse)
+
+        for bit in range(self.width):
+            if bit + shift >= len(shifted):
+                break
+            shifted[bit + shift] = self._vals[bit]
+
+        return LogicVector(self.width, shifted)
+
+    # TODO: This implementation is definitely bad and needs a rework when I'm less sleepy
+    def srl(self, shift: int) -> "LogicVector":
+        shifted = Logic.n_of_val(self.width, LV.BFalse)
+        rev = self._vals[::-1]
+
+        for bit in range(self.width):
+            if bit + shift >= len(shifted):
+                break
+            shifted[bit + shift] = rev[bit]
+
+        return LogicVector(self.width, shifted[::-1])
+
+    def __lshift__(self, by: int) -> "LogicVector":
+        return self.sll(by)
+
+    def __rshift__(self, by: int) -> "LogicVector":
+        return self.srl(by)
+
+    def __int__(self) -> int:
+        ac: int = 0
+        for bit in range(self.width):
+            if self._vals[bit]:
+                ac = ac + 2**bit
+        return ac
+
+    @staticmethod
+    def from_int(v: int, width: int = 0) -> "LogicVector":
+        bitwidth = width if width else v.bit_length()
+        ret = Logic.n_of_val(bitwidth, LV.BFalse)
+        for bit in range(bitwidth):
+            mask = 1 << bit
+            if (mask & v) != 0:
+                ret[bit] = L_TRUE
+        return LogicVector(bitwidth, ret)
